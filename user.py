6ede38e831f01fd181b4cb51c14a23239d6cd18b
CarlosRabat <107375637+CarlosRabat@users.noreply.github.com>
2024-02-04 09:47:58 -0600

    .env variables for API keys

--- user.py
+++ user.py
@@ -4,12 +4,21 @@ import base64
 import json
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+import dotenv
+import os
+
+
+# returns client id and client secret
+def load_client_info():
+    dotenv.load_dotenv()
+    client_id = os.getenv("CLIENT_ID")
+    client_secret = os.getenv("CLIENT_SECRET")
+    return client_id, client_secret
 
 
 # Grants Access to the spotify API
 def get_access_token():
-    client_id = "fcf64bb0ff3d47298a54df28088d4f75"
-    client_secret = "bab82d0743d74edcb3f18d24fc5953d3"
+    client_id, client_secret = load_client_info()
     token_url = "https://accounts.spotify.com/api/token"
     auth_headers = f"{client_id}:{client_secret}"
     encoded_auth_headers = base64.b64encode(auth_headers.encode()).decode()
@@ -39,8 +48,7 @@ def get_auth_header():
 
 # get the user for spotipy library
 def get_user():
-    client_id = "fcf64bb0ff3d47298a54df28088d4f75"
-    client_secret = "bab82d0743d74edcb3f18d24fc5953d3"
+    client_id, client_secret = load_client_info()
     client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)
     sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
     return sp
