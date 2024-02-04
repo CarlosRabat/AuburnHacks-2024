968937da1021c32b0d16bd363fb1eb0df5352967
CarlosRabat <107375637+CarlosRabat@users.noreply.github.com>
2024-02-03 22:04:41 -0600

    FastAPI Implementation

--- main.py
+++ main.py
@@ -2,17 +2,18 @@ from user import get_access_token, get_user
 from artist import search_for_artist_id, search_related_artist
 from playlist import search_for_playlist_id
 from track import get_track_ids_from_playlist
+from related_artist import related_artist
+from fastapi import FastAPI
 
-# from related_artist
-
-access_token = get_access_token()
+app = FastAPI()
 
 
-artist_id = search_for_artist_id("ACDC")
-related_artist = search_related_artist(artist_id)
-sp = get_user()
+@app.get("/")
+def read_root():
+    return {"message": "Hello, FastAPI!"}
 
-playlist_id = search_for_playlist_id("Top Global")
-ids = get_track_ids_from_playlist(playlist_id)
 
-print(related_artist)
+@app.get("/get_related_artist/{artist_name}")
+def get_related_artist(artist_name: str):
+    # artists = related_artist(artist_name)
+    return {"related_artists": "Artist1"}
